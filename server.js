4ab67084ea0425eac83294cf584874b2e10f5b2b
Lyly Long <lylylong@gmail.com>
2021-01-23 01:47:32 -0500

    testing Failed to load resource issues after deploy

--- server.js
+++ server.js
@@ -4,8 +4,8 @@ const mongoose = require("mongoose");
 const compression = require("compression");
 
 const PORT = process.env.PORT || 3001;
-const MONGODB_URI =
-  process.env.MONGODB_URI || "mongodb://localhost/expense-tracker";
+// const MONGODB_URI =
+//   process.env.MONGODB_URI || "mongodb://localhost/expense-tracker";
 
 const app = express();
 
@@ -17,14 +17,17 @@ app.use(express.json());
 
 app.use(express.static("public"));
 
-mongoose.connect(MONGODB_URI, {
-  useNewUrlParser: true,
-  useFindAndModify: false,
-  useUnifiedTopology: true,
-});
-
 // routes
 app.use(require("./routes/api"));
+
+mongoose.connect(
+  process.env.MONGODB_URI || "mongodb://localhost/expense-tracker",
+  {
+    useFindAndModify: false,
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  }
+);
 
 // Use this to log mongo queries being executed!
 mongoose.set("debug", true);
